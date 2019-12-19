568bb1e0ca8e9e33a3f4691f285dd7a3a70750f2
Alex-Danielsen <adanielsen447@gmail.com>
2019-12-19 09:28:47 -0500

    Removing files

--- Downloads/T744yr18_Houston/src/org/usfirst/frc/tm744yr18/robot/interfaces/TmCGrpCallbackI.java
+++
@@ -1,6 +0,0 @@
-package org.usfirst.frc.tm744yr18.robot.interfaces;
-
-public interface TmCGrpCallbackI {
-
-	boolean isOkToRunCmd();
-}
--- Downloads/T744yr18_Houston/src/org/usfirst/frc/tm744yr18/robot/interfaces/TmListEntryI.java
+++
@@ -1,7 +0,0 @@
-package org.usfirst.frc.tm744yr18.robot.interfaces;
-
-import java.util.List;
-
-public interface TmListEntryI<T> {
-	public List<T> getList();
-}
